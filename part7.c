5567ea404281d2ae00bd6e38966443e799f2d592
Don McLamb <mclamb.donald@gmail.com>
2016-03-21 10:26:18 -0400

    from work machine, moving

--- part7.c
+++ part7.c
@@ -3,14 +3,14 @@
 
 int main(int argc, char* argv[])
 {
-  char c = 'x';
-  char greeting[5] = {'h', 'e', 'l', 'l', 'o'};
+  char c[2] = {'x', '\0'};
+  char greeting[6] = {'h', 'e', 'l', 'l', 'o', '\0'};
   char farewell[] = {"bye"};
 
   printf("%s\n", greeting); //what is this going to print out?
 
-  printf("%s\n", &c); //how about this?
+  printf("%s\n", c); //how about this?
 
-  return(0);
+  // return(0);
 
 }
